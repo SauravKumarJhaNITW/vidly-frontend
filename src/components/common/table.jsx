b12b6a3247d74c39c976b8c948d929c8857a9228
SAURAV KUMAR JHA <mysauravnitw2019@outlook.com>
2021-07-02 19:27:54 +0530

    sorting implementation done

---
+++ src/components/common/table.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
+
+const Table = ({ columns, sortColumn, onSort, items }) => {
+  return (
+    <table className="table">
+      <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
+      <TableBody items={items} columns={columns} />
+    </table>
+  );
+};
+
+export default Table;
---
+++ src/components/moviesTable.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Like from "./common/like";
+import Table from "./common/table";
+
+class MoviesTable extends Component {
+  columns = [
+    { label: "Title", path: "title" },
+    { label: "Genre", path: "genre.name" },
+    { label: "Stock", path: "numberInStock" },
+    { label: "Rate", path: "dailyRentalRate" },
+    {
+      key: "like",
+      content: (movie) => (
+        <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
+      ),
+    },
+    {
+      key: "delete",
+      content: (movie) => (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      ),
+    },
+  ];
+
+  render() {
+    const { movies, sortColumn, onSort } = this.props;
+
+    return (
+      <Table
+        columns={this.columns}
+        sortColumn={sortColumn}
+        onSort={onSort}
+        items={movies}
+      />
+    );
+  }
+}
+
+export default MoviesTable;
